509a1042d3b5d2cb382ba5c36e150a13e93cb0ec
Victor Ask Justesen <2234-au704346@users.noreply.gitlab.au.dk>
2023-12-08 15:21:57 +0100

    Started mapping chars

--- Day8 (Scala)/day8/src/main/scala/Main.scala
+++ Day8 (Scala)/day8/src/main/scala/Main.scala
@@ -1,5 +1,21 @@
 import scala.io.Source
+import scala.collection.immutable.Queue
+
+def getInstructions(instStr: String): Queue[Char] =
+  return instStr.toCharArray()
+    .foldLeft(Queue.empty)((acc, c) => acc.appended(c))
+
+def getCoordinates(coordLines: List[String]): Map[String, (String, String)] =
+  return coordLines
+    .foldLeft(Map.empty[String, (String, String)])((mappedCoords, coord) =>
+      val from = coord.substring(0, 3)
+      
+    )
+
 def challenge1(lines: List[String]): Int = 
+  val instructions = getInstructions(lines.apply(0))
+
+  println(instructions)
   return 0
 
 def challenge2(lines: List[String]): Int = 
@@ -10,11 +26,10 @@ def runChallenges(lines: List[String]): Unit =
   println(challenge2(lines))
 
 def readInput(filepath: String): List[String] = 
-  return Source.fromFile(filepath).getLines().toList
+  return Source.fromResource(filepath).getLines().toList
 
 @main def hello: Unit =
-  val inputLines = readInput("/input.txt")
-  println(inputLines)
+  val inputLines = readInput("input.txt")
   runChallenges(inputLines)
 
 def msg = "I was compiled by Scala 3. :)"
